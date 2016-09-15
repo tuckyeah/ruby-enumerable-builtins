572d7475b68fcf48bcaca4df50977452ac996dfc
tuckyeah <h.rosebrock@gmail.com>
2016-09-15 11:31:39 -0400

    complete range_lab and range_list with exercises from class

--- bin/range_lab.rb
+++ bin/range_lab.rb
@@ -1,1 +1,11 @@
 # range as enumerable lab
+
+# calculate sum of all evens
+
+evens = (1..10).select(&:even?).reduce(:+)
+
+puts "#{evens}"
+
+odds = (1..10).select(&:odd?).reduce(:+)
+
+puts "#{odds}"
--- bin/range_list.rb
+++ bin/range_list.rb
@@ -1,1 +1,31 @@
 # range as enumerable code along
+
+ints = 0..10
+alpha = 'a'..'z'
+hex = 0x0..0xf
+
+ints.each do |n|
+  puts n
+end
+
+puts "maximum int is #{ints.max}"
+
+alpha.each do |a|
+  puts a
+end
+
+puts "maximum alpha is #{alpha.max}"
+
+hex.each do |h|
+  puts h
+end
+
+puts "maximum hex is #{hex.max}"
+
+every_third_letter = alpha.group_by do |a|
+  (a.ord % 3).zero? # converts letters to ASCII
+end
+
+puts "MYSTERY: #{every_third_letter}"
+
+# sentence = 'slow down i am not finished yet'
